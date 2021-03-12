34956ea85c730d804796071d96b3e6cbc72531fb
Emmanueleon <56766014+Emmanueleon@users.noreply.github.com>
2021-03-11 19:26:11 -0600

    Update and rename Course Project Script.R to run_analysis.R

--- Getting&Cleaning/run_analysis.R
+++ Getting&Cleaning/run_analysis.R
@@ -1,3 +1,7 @@
+# This script has the objective to download a file from the internet, to later clean the data set. 
+# This cleaned data set will be more easy to manipulate for further analysis
+
+#Prepare the enviroment
 library(plyr)
 library(tidyverse)
 
